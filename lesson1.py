c714e414c4f65a1af6c468aa6f6840ee939ff068
ilyanegusev0 <ilyanegusev0912@gmail.com>
2025-10-09 22:52:40 +0300

    upload 'lesson1'

---
+++ lesson1.py
@@ -0,0 +1,40 @@
+# LESSON 1:
+
+# print()
+print("\nprint()\n--------------------")
+print("\"", 1, 9, 8, 4, "\"", sep="", end="")
+print(" by George Orwell")
+
+#Math Operations
+print("\nMath Operations\n--------------------")
+print("1 + 6 =", 1 + 6)
+print("2 - 5 =", 2 - 5)
+print("3 * 4 =", 3 * 4)
+print("4 / 3 =", 4 / 3)
+print("4 // 3 =", 4 // 3)
+print("5 % 2 =", 5 % 2)
+print("6 ** 1 =", 6 ** 1)
+
+# min()
+print("\nmin()\n--------------------")
+print("min(50, 25, 0, -25, -50) =", min(50, 25, 0, -25, -50))
+
+# max()
+print("\nmax()\n--------------------")
+print("max(-50, -25, 0, 25, 50) =", max(-50, -25, 0, 25, 50))
+
+# abs()
+print("\nabs()\n--------------------")
+print("abs(-5) =", abs(-5))
+
+# pow()
+print("\npow()\n--------------------")
+print("pow(2, 8) =", pow(2, 8))
+
+# round()
+print("\nround()\n--------------------")
+print("round(256 / 10) =", round(256 / 10))
+
+# input()
+print("\ninput()\n--------------------")
+input("Print some text: ")
